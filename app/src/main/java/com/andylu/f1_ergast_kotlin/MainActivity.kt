44068d6db212e59fe6201cfb4c828840f630dad9
Andy <andyludeveloper@gmail.com>
2021-05-24 17:23:12 +0800

    Add fragment_data Add navigation

--- app/src/main/java/com/andylu/f1_ergast_kotlin/MainActivity.kt
+++ app/src/main/java/com/andylu/f1_ergast_kotlin/MainActivity.kt
@@ -2,10 +2,19 @@ package com.andylu.f1_ergast_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
     }
 }
