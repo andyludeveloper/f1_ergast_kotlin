2cf2f1e8ff1dde65e2cdeee8340db33998df20dc
Andy <andyludeveloper@gmail.com>
2021-05-27 18:13:50 +0800

    Change the api 1. use suspend function 2. Change the return type from Call to Response

--- app/src/main/java/com/andylu/f1_ergast_kotlin/service/F1ApiService.kt
+++ app/src/main/java/com/andylu/f1_ergast_kotlin/service/F1ApiService.kt
@@ -1,11 +1,11 @@
 package com.andylu.f1_ergast_kotlin.service
 
 import com.andylu.f1_ergast_kotlin.model.API
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface F1ApiService {
     @GET("f1/drivers.json")
-    fun getData(@Query("limit") limit:Int): Call<API>
+    suspend fun getData(@Query("limit") limit:Int): Response<API>
 }
