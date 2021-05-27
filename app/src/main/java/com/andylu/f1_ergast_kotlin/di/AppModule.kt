780fe4c50399dc1cf6dcc1b8fe09c7c8b4f03b40
Andy <andyludeveloper@gmail.com>
2021-05-27 20:12:55 +0800

    Use SingletonComponent in AppModule.kt

--- app/src/main/java/com/andylu/f1_ergast_kotlin/di/AppModule.kt
+++ app/src/main/java/com/andylu/f1_ergast_kotlin/di/AppModule.kt
@@ -5,13 +5,13 @@ import com.andylu.f1_ergast_kotlin.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
