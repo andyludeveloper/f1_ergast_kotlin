968a866a31d1031a0f452f8472ad52fb5e2dd5e5
Andy <andyludeveloper@gmail.com>
2021-05-27 20:14:22 +0800

    Inject view model

--- app/src/main/java/com/andylu/f1_ergast_kotlin/viewmodel/DriverViewModel.kt
+++ app/src/main/java/com/andylu/f1_ergast_kotlin/viewmodel/DriverViewModel.kt
@@ -4,23 +4,17 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.andylu.f1_ergast_kotlin.model.API
+import androidx.lifecycle.viewModelScope
 import com.andylu.f1_ergast_kotlin.model.Driver
-import com.andylu.f1_ergast_kotlin.service.F1ApiService
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.andylu.f1_ergast_kotlin.repository.F1DriverRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DriverViewModel : ViewModel() {
-    private val baseUrl = "https://ergast.com/api/"
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-    private val api = retrofit.create(F1ApiService::class.java)
-
+@HiltViewModel
+class DriverViewModel
+@Inject
+constructor(private val repository: F1DriverRepository) : ViewModel() {
     init {
         loadData()
     }
@@ -29,15 +23,14 @@ class DriverViewModel : ViewModel() {
     val data: LiveData<List<Driver>> = _data
 
     private fun loadData() {
-        api.getData(10).enqueue(object : Callback<API> {
-
-            override fun onResponse(call: Call<API>, response: Response<API>) {
-                _data.value = response.body()!!.mrData.driverTable.drivers
+        viewModelScope.launch {
+            repository.getF1Drivers().let { response ->
+                if (response.isSuccessful) {
+                    _data.postValue(response.body()!!.mrData.driverTable.drivers)
+                } else {
+                    Log.d("DataViewModel", "onFailure: ${response.message()}")
+                }
             }
-
-            override fun onFailure(call: Call<API>, t: Throwable) {
-                Log.d("DataViewModel", "onFailure: ${t.message}")
-            }
-        })
+        }
     }
 }
